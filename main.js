a6047351830f9bfc075d5d12e68d0fd9dcd89655
hjpotter92 <git@hjpotter92.email>
2023-03-30 13:34:18 +0530

    Setup project to be dockerised.

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-const { Client, Events } = require('pg')
+const { Client, Events } = require("pg");
 const Discord = require("discord.js");
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 
 const bot = new Discord.Client({
-	intents: [
-		Discord.GatewayIntentBits.Guilds,
-		Discord.GatewayIntentBits.GuildMessages,
-		Discord.GatewayIntentBits.MessageContent
-	]});
-
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.MessageContent,
+  ],
+});
 
 const db = new Client({
-  host:process.env.DB_HOST,
-  port:process.env.DB_PORT,
-  database:process.env.DB_NAME,
-  user:process.env.DB_USER,
-  password:process.env.DB_PASSWORD,
-})
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
 
 // create table discord (
 // 	msg_time    numeric,
@@ -35,66 +35,74 @@ let db_conn = false;
 
 db.connect((err) => {
   if (err) {
-    console.error('PostgreSQL error: ', err.toString());
+    console.error("PostgreSQL error: ", err.toString());
   } else {
-    console.log('Connected to DB');
+    console.log("Connected to DB");
     db_conn = true;
   }
-})
-
+});
 
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
   bot.on("ready", () => {
     console.log("Connected to Discord");
-    console.log(bot.guilds.cache.map((guild) => guild.name).join('\n'))
+    console.log(bot.guilds.cache.map((guild) => guild.name).join("\n"));
   });
-  
+
   bot.on("error", (e) => {
-    console.error("Discord error: "+e.toString());
+    console.error("Discord error: " + e.toString());
   });
-  
+
   bot.on("messageCreate", (msg) => {
     //Docs: https://discord.js.org/#/docs/discord.js/main/class/Message
     // You probably want msg.author.username (string) and msg.author.id (a "snowflake", basically a massive number, but best to treat like a string since most databases can't handle numbers this massive well) for "who sent it"
     // (the ID is constant, the username can change at any time!)
     // msg.content has the message contents (e.g. text) as a string
     // msg.createdTimestamp has the message creation time (integer)
-  
+
     // Make sure DB is connected before we start inserting
-    msg.guild.members.fetch(msg.author).then((m) => {
-      let roles = [];
-      for (const role of m.roles.cache){
-        roles.push(role[1].name);
-      }
-      if (db_conn){
-        console.log('writing to db', [
-          Number(msg.createdTimestamp), 
-          Number(msg.author.id), 
-          msg.author.username, 
-          msg.content,
-          msg.channel.name, 
-          Number(msg.channelId),
-          roles,
-          msg.guild.name
-        ].join(", "))
-        db.query(
-          'INSERT INTO discord (msg_time, author_id, author_name, msg, channel, channel_id, roles, server_name) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', 
-          [
-            Number(msg.createdTimestamp), 
-            Number(msg.author.id), 
-            msg.author.username, 
-            msg.content,
-            msg.channel.name, 
-            Number(msg.channelId),
-            roles,
-            msg.guild.name
-          ]
-          ).then((result) => {}).catch((e) => console.error(e.stack));
-      }
-    }).then((result) => {}).catch((e) => console.error(e.stack));  
+    msg.guild.members
+      .fetch(msg.author)
+      .then((m) => {
+        let roles = [];
+        for (const role of m.roles.cache) {
+          roles.push(role[1].name);
+        }
+        if (db_conn) {
+          console.log(
+            "writing to db",
+            [
+              Number(msg.createdTimestamp),
+              Number(msg.author.id),
+              msg.author.username,
+              msg.content,
+              msg.channel.name,
+              Number(msg.channelId),
+              roles,
+              msg.guild.name,
+            ].join(", ")
+          );
+          db.query(
+            "INSERT INTO discord (msg_time, author_id, author_name, msg, channel, channel_id, roles, server_name) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+            [
+              Number(msg.createdTimestamp),
+              Number(msg.author.id),
+              msg.author.username,
+              msg.content,
+              msg.channel.name,
+              Number(msg.channelId),
+              roles,
+              msg.guild.name,
+            ]
+          )
+            .then((result) => {})
+            .catch((e) => console.error(e.stack));
+        }
+      })
+      .then((result) => {})
+      .catch((e) => console.error(e.stack));
   });
-  
+
   bot.login(process.env.BOT_TOKEN).catch(console.error);
 });

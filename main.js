e82f74e3478335ff2ab75baac442bc376bb41cac
Evan Mullins <evancmullins@gmail.com>
2023-07-24 16:38:28 -0500

    restructured to handle problems with roles

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const bot = new Discord.Client({
     Discord.GatewayIntentBits.Guilds,
     Discord.GatewayIntentBits.GuildMessages,
     Discord.GatewayIntentBits.MessageContent,
+    Discord.GatewayIntentBits.GuildMembers,
   ],
 });
 
@@ -47,7 +48,7 @@ app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
   bot.on("ready", () => {
     console.log("Connected to Discord");
-    console.log(bot.guilds.cache.map((guild) => guild.name).join("\n"));
+    console.log(bot.guilds.cache.map((guild) => guild.id).join("\n"));
   });
 
   bot.on("error", (e) => {
@@ -62,48 +63,42 @@ app.listen(port, () => {
     // msg.createdTimestamp has the message creation time (integer)
     
     // Make sure DB is connected before we start inserting
-    console.log("messageCreate", msg.author.username, msg.content, msg.channel.name, msg.channelId, msg.guild.name);
-    msg.guild.members
+    roles = msg.guild.members
       .fetch(msg.author)
       .then((m) => {
         let roles = [];
         for (const role of m.roles.cache) {
           roles.push(role[1].name);
-        }
-        if (db_conn) {
-          console.log(
-            "writing to db",
-            [
-              Number(msg.createdTimestamp),
-              Number(msg.author.id),
-              msg.author.username,
-              msg.content,
-              msg.channel.name,
-              Number(msg.channelId),
-              roles,
-              msg.guild.name,
-            ].join(", ")
-          );
-          db.query(
-            "INSERT INTO discord (msg_time, author_id, author_name, msg, channel, channel_id, roles, server_name) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
-            [
-              Number(msg.createdTimestamp),
-              Number(msg.author.id),
-              msg.author.username,
-              msg.content,
-              msg.channel.name,
-              Number(msg.channelId),
-              roles,
-              msg.guild.name,
-            ]
-          )
-            .then((result) => {})
-            .catch((e) => console.error(e.stack));
-        }
-      })
-      .then((result) => {})
-      .catch((e) => console.error(e.stack));
-  });
+        }})
+        .catch((e) => {
+          console.error("missing roles", e.stack)
+          return null})
+
+    console.log("messageCreate", msg.author.username, msg.content, msg.channel.name, msg.channelId, msg.guild.name, roles);
+    
+    row = [
+      Number(msg.createdTimestamp),
+      Number(msg.author.id),
+      msg.author.username,
+      msg.content,
+      msg.channel.name,
+      Number(msg.channelId),
+      roles,
+      msg.guild.name,
+    ]
+    if (db_conn) {
+      console.log(
+        "writing to db",
+        row.join(", ")
+      );
+      db.query(
+        "INSERT INTO discord (msg_time, author_id, author_name, msg, channel, channel_id, roles, server_name) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+        row
+      )
+        .then((result) => {})
+        .catch((e) => console.error(e.stack));
+    }
+  })
 
   bot.login(process.env.BOT_TOKEN).catch(console.error);
 });

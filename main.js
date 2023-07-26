e537799633dd4e9a5175994ac343f287bbaf2e4e
Evan Mullins <evancmullins@gmail.com>
2023-07-26 12:24:34 -0500

    fixed unresolved promise. wrote log on success

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ app.listen(port, () => {
     // (the ID is constant, the username can change at any time!)
     // msg.content has the message contents (e.g. text) as a string
     // msg.createdTimestamp has the message creation time (integer)
-    
+
     // Make sure DB is connected before we start inserting
     roles = msg.guild.members
       .fetch(msg.author)
@@ -69,35 +69,38 @@ app.listen(port, () => {
         let roles = [];
         for (const role of m.roles.cache) {
           roles.push(role[1].name);
-        }})
-        .catch((e) => {
-          console.error("missing roles", e.stack)
-          return null})
+        }
+        return roles;
+      })
+      .catch((e) => {
+        console.error("missing roles", e.stack)
+        return null
+      })
 
-    console.log("messageCreate", msg.author.username, msg.content, msg.channel.name, msg.channelId, msg.guild.name, roles);
-    
-    row = [
-      Number(msg.createdTimestamp),
-      Number(msg.author.id),
-      msg.author.username,
-      msg.content,
-      msg.channel.name,
-      Number(msg.channelId),
-      roles,
-      msg.guild.name,
-    ]
-    if (db_conn) {
-      console.log(
-        "writing to db",
-        row.join(", ")
-      );
-      db.query(
-        "INSERT INTO discord (msg_time, author_id, author_name, msg, channel, channel_id, roles, server_name) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
-        row
-      )
-        .then((result) => {})
-        .catch((e) => console.error(e.stack));
-    }
+    Promise.allSettled([roles]).then((values) => {
+      row = [
+        Number(msg.createdTimestamp),
+        Number(msg.author.id),
+        msg.author.username,
+        msg.content,
+        msg.channel.name,
+        Number(msg.channelId),
+        values[0].value,
+        msg.guild.name,
+      ]
+      if (db_conn) {
+        console.log(
+          "writing to db",
+          row.join(", ")
+        );
+        db.query(
+          "INSERT INTO discord (msg_time, author_id, author_name, msg, channel, channel_id, roles, server_name) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+          row
+        )
+          .then(() => { console.log("message created successfully", row.join(", ")); })
+          .catch((e) => console.error(e.stack));
+      }
+    })
   })
 
   bot.login(process.env.BOT_TOKEN).catch(console.error);
